480963820e8bb64eb7d82ac9b923223d4e8c1326
Kishore Kanala <kishorekanala@gmail.com>
2021-10-31 00:25:48 +0530

    Added initial action for Add

--- Groceries/app/src/main/java/com/kishore/groceries/MainActivity.kt
+++ Groceries/app/src/main/java/com/kishore/groceries/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.kishore.groceries
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+
+val TAG = "GRO"
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val addButton: Button = findViewById<Button>(R.id.btn_Add)
+
+        addButton.setOnClickListener(object: View.OnClickListener {
+            override fun onClick(v: View?) {
+                //TODO("Not yet implemented")
+                Log.d(TAG, "Adding new item")
+
+                val intent = Intent(this@MainActivity, GroceriesAddActivity::class.java)
+                startActivity(intent)
+            }
+        })
+
     }
 }
