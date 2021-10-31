88fa1eb6a5aca2ffd3edec6826c08794477d2586
Kishore Kanala <kishorekanala@gmail.com>
2021-10-31 10:40:03 +0530

    Added actions to OK, Cancel

--- Groceries/app/src/main/java/com/kishore/groceries/GroceriesAddEditActivity.kt
+++ Groceries/app/src/main/java/com/kishore/groceries/GroceriesAddEditActivity.kt
@@ -1,11 +1,39 @@
 package com.kishore.groceries
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
 
 class GroceriesAddEditActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_groceries_addedit)
+
+        val btnOK: Button = findViewById<Button>(R.id.btn_Ok)
+        val btnCancel: Button = findViewById<Button>(R.id.btn_Cancel)
+
+        btnOK.setOnClickListener(object: View.OnClickListener {
+            override fun onClick(v: View?) {
+                //TODO("Not yet implemented")
+                Log.d(TAG, "Everything Ok.. Accept")
+
+                val intent = Intent(this@GroceriesAddEditActivity, MainActivity::class.java)
+                startActivity(intent)
+            }
+        })
+
+        btnCancel.setOnClickListener(object: View.OnClickListener {
+            override fun onClick(v: View?) {
+                //TODO("Not yet implemented")
+                Log.d(TAG, "Not Ok.. Reject")
+
+                val intent = Intent(this@GroceriesAddEditActivity, MainActivity::class.java)
+                startActivity(intent)
+            }
+        })
+
     }
 }
